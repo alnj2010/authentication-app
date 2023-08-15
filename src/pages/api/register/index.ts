fe5666708282df81a44d70b7927fa46a5610c30b
Abraham Navarro <abrahancho6@gmail.com>
2023-08-15 14:11:50 -0400

    feat: adding register tests

--- src/pages/api/register/index.ts
+++ src/pages/api/register/index.ts
@@ -1,9 +1,16 @@
-import { REGISTER_SERVICE_ERROR_EXISTING_USER } from "@/domain/constants";
-import { CustomResponse } from "@/domain/types";
+import { REGISTER_SERVICE_ERROR_EXISTING_USER, SERVICE_ERROR_INTERNAL } from "@/domain/constants";
+import { AuthInfo, CustomResponse } from "@/domain/types";
 import { NextApiRequest, NextApiResponse } from "next";
 import UserRepository from "@/repositories/user-repository";
 import TokenUtil from "@/lib/token";
 import CookieUtil from "@/lib/cookie";
+import {
+  emailPatternValidator,
+  min4CharsValidator,
+  nonEmptyValidator,
+  validateScheme,
+} from "@/lib/validator";
+import { FormValidationError } from "@/domain/errors/form-validation-error";
 
 export default async function handler(
   req: NextApiRequest,
@@ -12,24 +19,52 @@ export default async function handler(
   if (req.method === "POST") {
     const { email, password } = req.body;
 
-    const userExists = await UserRepository.doesUserEmailExist(email);
-    if (userExists) {
-      res.status(400).json({
-        code: 400,
-        error: REGISTER_SERVICE_ERROR_EXISTING_USER,
-        data: null,
-      });
-    } else {
-      await UserRepository.createUserByCredentials({ email, password });
+    const authInfoScheme = {
+      email: {
+        value: email,
+        validators: [nonEmptyValidator, emailPatternValidator],
+      },
+      password: {
+        value: password,
+        validators: [nonEmptyValidator, min4CharsValidator],
+      },
+    };
 
-      const user = await UserRepository.getUserByEmail(email);
+    try {
+      validateScheme(authInfoScheme);
 
-      const token = TokenUtil.createToken(user.id);
+      const userExists = await UserRepository.doesUserEmailExist(email);
+      if (userExists) {
+        res.status(400).json({
+          code: 400,
+          error: REGISTER_SERVICE_ERROR_EXISTING_USER,
+          data: null,
+        });
+      } else {
+        await UserRepository.createUserByCredentials({ email, password });
 
-      const cookie = CookieUtil.serializeAccessToken(token);
+        const user = await UserRepository.getUserByEmail(email);
 
-      res.setHeader("Set-Cookie", cookie);
-      res.status(200).json({ code: 200, data: "Successfully Registration" });
+        const token = TokenUtil.createToken(user.id);
+
+        const cookie = CookieUtil.serializeAccessToken(token);
+
+        res.setHeader("Set-Cookie", cookie);
+        res.status(200).json({ code: 200, data: "Successfully Registration" });
+      }
+    } catch (error) {
+      if (error instanceof FormValidationError)
+        res.status(400).json({
+          code: 400,
+          error: error.errorMsgs.join(" "),
+          data: null,
+        });
+      else
+        res.status(500).json({
+          code: 500,
+          error: SERVICE_ERROR_INTERNAL,
+          data: null,
+        });
     }
   }
 }
---
+++ test/functional/server/register.spec.ts
@@ -0,0 +1,92 @@
+import { createMocks } from "node-mocks-http";
+import handler from "@/pages/api/register";
+import { userDummy } from "../../dummies";
+import UserRepository from "@/repositories/user-repository";
+import { invalidFieldMsg, lessThan4CharsFieldMsg } from "@/lib/validator";
+import { REGISTER_SERVICE_ERROR_EXISTING_USER } from "@/domain/constants";
+
+jest.mock("@/repositories/user-repository.ts", () => {
+  return {
+    getUserByEmail: jest.fn(),
+    doesUserEmailExist: jest.fn().mockRejectedValue(false),
+    createUserByCredentials: jest.fn(),
+    getUserById: jest.fn(),
+  };
+});
+
+describe("endpoint POST /register", () => {
+  beforeEach(() => {
+    (UserRepository.getUserByEmail as jest.Mock).mockReset();
+    (UserRepository.doesUserEmailExist as jest.Mock).mockReset();
+    (UserRepository.createUserByCredentials as jest.Mock).mockReset();
+    (UserRepository.getUserById as jest.Mock).mockReset();
+  });
+
+  it("Should return code 400 when invalid email or password is submitted", async () => {
+    jest.spyOn(UserRepository, "getUserByEmail").mockResolvedValue(userDummy);
+
+    const body = { email: "emailinvalid", password: "*" };
+    const { req, res } = createMocks({
+      method: "POST",
+      body,
+    });
+
+    // @ts-ignore
+    await handler(req, res);
+    expect(res.statusCode).toBe(400);
+    expect(res._getJSONData().error).toContain(invalidFieldMsg("email"));
+    expect(res._getJSONData().error).toContain(
+      lessThan4CharsFieldMsg("password")
+    );
+  });
+
+  it("Should return code 400 when email already exist in DB", async () => {
+    jest.spyOn(UserRepository, "doesUserEmailExist").mockResolvedValue(true);
+
+    const body = { email: userDummy.email, password: userDummy.password };
+    const { req, res } = createMocks({
+      method: "POST",
+      body,
+    });
+
+    // @ts-ignore
+    await handler(req, res);
+    expect(res.statusCode).toBe(400);
+    expect(res._getJSONData().error).toContain(
+      REGISTER_SERVICE_ERROR_EXISTING_USER
+    );
+  });
+
+  it("Should return code 500 when DB is fail", async () => {
+    jest
+      .spyOn(UserRepository, "doesUserEmailExist")
+      .mockRejectedValue(new Error("error"));
+
+    const body = { email: userDummy.email, password: userDummy.password };
+    const { req, res } = createMocks({
+      method: "POST",
+      body,
+    });
+
+    // @ts-ignore
+    await handler(req, res);
+    expect(res.statusCode).toBe(500);
+  });
+
+  it("Should return code 200 when email and password are submited sucessfully", async () => {
+    jest.spyOn(UserRepository, "getUserByEmail").mockResolvedValue(userDummy);
+
+    const body = { email: userDummy.email, password: userDummy.password };
+    const { req, res } = createMocks({
+      method: "POST",
+      body,
+    });
+
+    // @ts-ignore
+    await handler(req, res);
+
+    expect(UserRepository.createUserByCredentials).toHaveBeenCalledWith(body);
+    expect(res.getHeader("Set-Cookie")).toBeDefined();
+    expect(res.statusCode).toBe(200);
+  });
+});
